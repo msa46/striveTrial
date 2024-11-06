650b9c0c59b093e465d745f3b8b7ec81cae76c57
msa46 <mohamad.sanaye462@gmail.com>
2024-11-06 14:52:03 +0100

    Add first basic form of navigation

---
+++ app/(tabs)/planner.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+const  planner = () => (
+    <View style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:"black"}}>
+        <Text className='color-white'>Another route</Text>
+    </View>
+)
+
+export default planner;
